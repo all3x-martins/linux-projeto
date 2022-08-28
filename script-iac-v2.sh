40f430f2149d5522f2f66d32e2e51ed080d6e462
Allexander Martins <85321713+all3x-martins@users.noreply.github.com>
2022-08-28 14:13:41 -0300

    Apache-v1
    Script para provisionamento de um servidor Apache

---
+++ script-iac-v2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+
+apt-get upgrade -y
+
+echo "Concluido!!"
+
+echo "Instalando aplicacoes..."
+
+apt-get install apache2 -y
+
+apt-get install unzip -y
+
+echo "Concluido!!"
+
+echo "Realizando configuracoes..."
+
+cd /tmp
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cd linux-site-dio-main
+
+cp -R * var/www/html
+
+echo "Concluido!!"
+
